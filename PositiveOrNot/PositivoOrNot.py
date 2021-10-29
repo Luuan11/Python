c2b28fc97aa9bf64f5b2078ccabd7bcb3a0efa4a
Luan Fernando <79935555+Luuan11@users.noreply.github.com>
2021-10-28 21:21:41 -0300

    Upando py projeto

---
+++ PositiveOrNot/PositivoOrNot.py
@@ -0,0 +1,8 @@
+num = int(input("Digite um número:"));
+
+if num <0:
+    print("O valor digitado é negativo");
+elif num == 0:
+    print("O valor digitado é Zero");
+else:
+    print("O valor digitado é positivo");
